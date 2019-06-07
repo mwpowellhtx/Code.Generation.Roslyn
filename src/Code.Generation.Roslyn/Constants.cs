5144c897add96dd21414557033057043230ff34a
Michael W Powell <mwpowellhtx@gmail.com>
2019-06-06 21:08:59 -0400

    updated constants

--- src/Code.Generation.Roslyn/Constants.cs
+++ src/Code.Generation.Roslyn/Constants.cs
@@ -18,5 +18,59 @@
         /// <see cref="CarriageReturn"/>
         /// <see cref="LineFeed"/>
         public const string CarriageReturnLineFeed = CarriageReturn + LineFeed;
+
+        /// <summary>
+        /// The pair of Open- and Close-CurlyBraces, literally &quot;{}&quot;
+        /// </summary>
+        public const string CurlyBraces = "{}";
+
+        /// <summary>
+        /// Gets the OpenCurlyBrace, literally &apos;{&apos;.
+        /// </summary>
+        /// <see cref="CurlyBraces"/>
+        public static char OpenCurlyBrace => CurlyBraces[0];
+
+        /// <summary>
+        /// Gets the CloseCurlyBrace, literally &apos;}&apos;.
+        /// </summary>
+        /// <see cref="CurlyBraces"/>
+        public static char CloseCurlyBrace => CurlyBraces[1];
+
+        /// <summary>
+        /// The pair of Open- and Close-SquareBrackets, literally &quot;[]&quot;
+        /// </summary>
+        public const string SquareBrackets = "[]";
+
+        /// <summary>
+        /// Gets the OpenSquareBracket, literally &apos;[&apos;.
+        /// </summary>
+        /// <see cref="SquareBrackets"/>
+        public static char OpenSquareBracket => SquareBrackets[0];
+
+        /// <summary>
+        /// Gets the CloseSquareBracket, literally &apos;]&apos;.
+        /// </summary>
+        /// <see cref="SquareBrackets"/>
+        public static char CloseSquareBracket => SquareBrackets[1];
+
+        /// <summary>
+        /// &apos;:&apos;
+        /// </summary>
+        public const char Colon = ':';
+
+        /// <summary>
+        /// &apos;;&apos;
+        /// </summary>
+        public const char SemiColon = ';';
+
+        /// <summary>
+        /// &quot;using&quot;
+        /// </summary>
+        public static string Using = nameof(Using).ToLower();
+
+        /// <summary>
+        /// &quot;namespace&quot;
+        /// </summary>
+        public static string Namespace = nameof(Namespace).ToLower();
     }
 }
