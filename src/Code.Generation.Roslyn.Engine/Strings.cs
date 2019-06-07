9534d5a544c6e126b95eedc7fc110bd9887ce427
Michael W Powell <mwpowellhtx@gmail.com>
2019-06-06 21:11:09 -0400

    added strings helper bits

---
+++ src/Code.Generation.Roslyn.Engine/Strings.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Code.Generation.Roslyn
+{
+    using static String;
+
+    /// <summary>
+    /// Strings static helpers class.
+    /// </summary>
+    internal static class Strings
+    {
+        /// <summary>
+        /// Returns whether <paramref name="s"/> is Not <see cref="IsNullOrEmpty"/>.
+        /// </summary>
+        /// <param name="s"></param>
+        /// <returns></returns>
+        public static bool IsNotNullOrEmpty(string s) => !IsNullOrEmpty(s);
+    }
+}
