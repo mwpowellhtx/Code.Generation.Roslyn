adff09bedd0fb561be97a6256e78728c25d04857
Michael W Powell <mwpowellhtx@gmail.com>
2019-06-06 21:18:40 -0400

    added domain static helpers

---
+++ src/Code.Generation.Roslyn.Tests/Extensions/Domain.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace Code.Generation.Roslyn
+{
+    internal static class Domain
+    {
+        // ReSharper disable InconsistentNaming
+        /// <summary>
+        /// &quot;shorthand&quot;
+        /// </summary>
+        public const string shorthand = nameof(shorthand);
+
+        /// <summary>
+        /// &quot;full_name&quot;
+        /// </summary>
+        public const string full_name = nameof(full_name);
+
+        /// <summary>
+        /// &quot;assembly&quot;
+        /// </summary>
+        public const string assembly = nameof(assembly);
+        // ReSharper restore InconsistentNaming
+
+        /// <summary>
+        /// &quot;Attribute&quot;
+        /// </summary>
+        /// <see cref="Attribute"/>
+        public const string AttributeName = nameof(Attribute);
+    }
+}
