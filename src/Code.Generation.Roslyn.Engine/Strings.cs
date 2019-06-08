b3e758735a00a7c0f2931a05f1a355620ef1d385
Michael W Powell <mwpowellhtx@gmail.com>
2019-06-08 13:35:20 -0400

    added strings extension method

--- src/Code.Generation.Roslyn.Engine/Strings.cs
+++ src/Code.Generation.Roslyn.Engine/Strings.cs
@@ -15,5 +15,15 @@ namespace Code.Generation.Roslyn
         /// <param name="s"></param>
         /// <returns></returns>
         public static bool IsNotNullOrEmpty(string s) => !IsNullOrEmpty(s);
+
+        /// <summary>
+        /// Returns the <paramref name="values"/> <see cref="string.Join(string,string[])"/>
+        /// by the <paramref name="separator"/>. Since this is an extension method, the
+        /// <paramref name="separator"/> anchors its invocation.
+        /// </summary>
+        /// <param name="separator"></param>
+        /// <param name="values"></param>
+        /// <returns></returns>
+        public static string Join(this string separator, params string[] values) => string.Join(separator, values);
     }
 }
