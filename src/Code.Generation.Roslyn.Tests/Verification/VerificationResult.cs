a353cf7a23fb705a3a8fd78b9f755b686b782d0d
Michael W Powell <mwpowellhtx@gmail.com>
2019-06-08 13:38:56 -0400

    added verification result enumeration

---
+++ src/Code.Generation.Roslyn.Tests/Verification/VerificationResult.cs
@@ -0,0 +1,58 @@
+﻿namespace Code.Generation.Roslyn
+{
+    /// <summary>
+    /// 
+    /// </summary>
+    public enum VerificationResult
+    {
+        /// <summary>
+        /// 
+        /// </summary>
+        LengthMismatch,
+
+        /// <summary>
+        /// 
+        /// </summary>
+        DiagnosticMismatch,
+
+        /// <summary>
+        /// 
+        /// </summary>
+        LocationMismatch,
+
+        /// <summary>
+        /// 
+        /// </summary>
+        IdMismatch,
+
+        /// <summary>
+        /// 
+        /// </summary>
+        SeverityMismatch,
+
+        /// <summary>
+        /// 
+        /// </summary>
+        MessageMismatch,
+
+        /// <summary>
+        /// 
+        /// </summary>
+        PathMismatch,
+
+        /// <summary>
+        /// 
+        /// </summary>
+        LineMismatch,
+
+        /// <summary>
+        /// 
+        /// </summary>
+        ColumnMismatch,
+
+        /// <summary>
+        /// 
+        /// </summary>
+        BadLocation
+    }
+}
