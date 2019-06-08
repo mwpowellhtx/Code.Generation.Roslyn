4b778288945f448d6808d96394cb8db332fd1041
Michael W Powell <mwpowellhtx@gmail.com>
2019-06-08 13:38:44 -0400

    added diagnostic predicate

---
+++ src/Code.Generation.Roslyn.Tests/Verification/DiagnosticFilterPredicate.cs
@@ -0,0 +1,12 @@
+﻿namespace Code.Generation.Roslyn
+{
+    using Microsoft.CodeAnalysis;
+
+    /// <summary>
+    /// Represents a <paramref name="diagnostic"/> Predicate for the
+    /// <see cref="CompilationDiagnosticFilter{TCompilation}"/>.
+    /// </summary>
+    /// <param name="diagnostic"></param>
+    /// <returns></returns>
+    public delegate bool DiagnosticFilterPredicate(Diagnostic diagnostic);
+}
