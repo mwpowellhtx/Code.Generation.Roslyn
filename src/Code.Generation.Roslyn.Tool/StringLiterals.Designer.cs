a68a8d734ff76f9dec0b9983a1a1d592d427c9f8
Michael W Powell <mwpowellhtx@gmail.com>
2019-06-12 15:50:45 -0400

    renamed the string literals assets to avoid name collisions

--- src/Code.Generation.Roslyn.Tool/StringLiterals.Designer.cs
+++ src/Code.Generation.Roslyn.Tool/StringLiterals.Designer.cs
@@ -22,14 +22,14 @@ namespace Code.Generation.Roslyn {
     [global::System.CodeDom.Compiler.GeneratedCodeAttribute("System.Resources.Tools.StronglyTypedResourceBuilder", "15.0.0.0")]
     [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
     [global::System.Runtime.CompilerServices.CompilerGeneratedAttribute()]
-    internal class Strings {
+    internal class StringLiterals {
         
         private static global::System.Resources.ResourceManager resourceMan;
         
         private static global::System.Globalization.CultureInfo resourceCulture;
         
         [global::System.Diagnostics.CodeAnalysis.SuppressMessageAttribute("Microsoft.Performance", "CA1811:AvoidUncalledPrivateCode")]
-        internal Strings() {
+        internal StringLiterals() {
         }
         
         /// <summary>
@@ -39,7 +39,7 @@ namespace Code.Generation.Roslyn {
         internal static global::System.Resources.ResourceManager ResourceManager {
             get {
                 if (object.ReferenceEquals(resourceMan, null)) {
-                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("Code.Generation.Roslyn.Strings", typeof(Strings).Assembly);
+                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("Code.Generation.Roslyn.StringLiterals", typeof(StringLiterals).Assembly);
                     resourceMan = temp;
                 }
                 return resourceMan;
