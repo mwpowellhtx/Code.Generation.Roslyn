549ef6d87b196854b18b8af310cf682d8854b65d
Michael W Powell <mwpowellhtx@gmail.com>
2019-06-12 15:51:16 -0400

    extended the entry point approach a little bit approaching more comprehensive unit testing

--- src/Code.Generation.Roslyn.Tool/Program.cs
+++ src/Code.Generation.Roslyn.Tool/Program.cs
@@ -1,13 +1,28 @@
 ﻿using System;
+using System.IO;
 
 namespace Code.Generation.Roslyn
 {
     using NConsole.Options;
-    using static Console;
 
     internal static class Program
     {
-        private static int Main(string[] args)
+        /// <summary>
+        /// Gets the Out <see cref="TextWriter"/>.
+        /// </summary>
+        public static TextWriter Out { get; set; } = Console.Out;
+
+        /// <summary>
+        /// Gets the Error <see cref="TextWriter"/>.
+        /// </summary>
+        public static TextWriter Error { get; set; } = Console.Error;
+
+        /// <summary>
+        /// The Main method Program entry point.
+        /// </summary>
+        /// <param name="args"></param>
+        /// <returns></returns>
+        public static int Main(string[] args)
         {
             using (var toolConsoleManager = new ToolConsoleManager(Out, Error))
             {
