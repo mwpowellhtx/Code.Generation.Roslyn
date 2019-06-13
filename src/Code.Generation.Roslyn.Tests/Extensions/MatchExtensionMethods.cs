8025f18c926e35ccdbb5204aed23f9c4e5671429
Michael W Powell <mwpowellhtx@gmail.com>
2019-06-13 16:44:48 -0400

    added regular expression match extension methods

---
+++ src/Code.Generation.Roslyn.Tests/Extensions/MatchExtensionMethods.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Linq;
+using System.Text.RegularExpressions;
+
+namespace Code.Generation.Roslyn
+{
+    using static String;
+
+    internal static class MatchExtensionMethods
+    {
+        public static bool HasGroup(this Match match, string groupName) => match.Groups.Any(x => x.Name == groupName);
+
+        public static string GetGroupValue(this Match match, string groupName) => match.Groups[groupName]?.Value;
+
+        public static bool TryGetGroupValue(this Match match, string groupName, out string s) => !IsNullOrEmpty(
+            s = match.HasGroup(groupName) ? GetGroupValue(match, groupName) : null
+        );
+    }
+}
