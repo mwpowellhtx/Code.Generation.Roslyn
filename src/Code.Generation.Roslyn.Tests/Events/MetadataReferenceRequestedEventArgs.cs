b0c2e958450a7a7e3db772008fe52005436abe28
Michael W Powell <mwpowellhtx@gmail.com>
2019-06-12 23:25:12 -0400

    refactored key bits of event arguments leveraging the interface

--- src/Code.Generation.Roslyn.Tests/Events/MetadataReferenceRequestedEventArgs.cs
+++ src/Code.Generation.Roslyn.Tests/Events/MetadataReferenceRequestedEventArgs.cs
@@ -14,8 +14,10 @@ namespace Code.Generation.Roslyn
     /// Use the <see cref="ResolveMetadataReferencesEventArgs"/> members in order to
     /// specify the requisite <see cref="MetadataReference"/> instances.
     /// </summary>
-    /// <inheritdoc />
-    public class ResolveMetadataReferencesEventArgs : EventArgs
+    /// <inheritdoc cref="ICanReferenceAssembly{T}"/>
+    public class ResolveMetadataReferencesEventArgs
+        : EventArgs
+            , ICanReferenceAssembly<ResolveMetadataReferencesEventArgs>
     {
         /// <summary>
         /// Gets the Solution involved in the <see cref="MetadataReference"/> Resolution.
@@ -91,17 +93,7 @@ namespace Code.Generation.Roslyn
         /// <returns></returns>
         private static Assembly GetTypeAssembly<T>() => typeof(T).GetTypeInfo().Assembly;
 
-        /// <summary>
-        /// Adds References to the <paramref name="assetRelativeNames"/>, usually Assembly
-        /// Dynamic Link Libraries (DLLs), but may be dotnet Executable (EXE) as well. Uses
-        /// the <typeparamref name="T"/> host <see cref="Assembly.Location"/>
-        /// <see cref="GetDirectoryName(string)"/> as the base directory.
-        /// </summary>
-        /// <typeparam name="T"></typeparam>
-        /// <param name="assetRelativeNames"></param>
-        /// <returns></returns>
-        /// <see cref="!:https://stackoverflow.com/questions/23907305#47196516">Roslyn has no reference to System.Runtime</see>
-        /// <remarks>This method is provided as a workaround to the references System.Runtime issue.</remarks>
+        /// <inheritdoc />
         public ResolveMetadataReferencesEventArgs AddTypeAssemblyLocationBasedReferences<T>(params string[] assetRelativeNames)
         {
             var typeAssemblyLocation = GetTypeAssembly<T>().Location;
@@ -109,20 +101,10 @@ namespace Code.Generation.Roslyn
             return assetRelativeNames.Select(x => Combine(locationDirectoryName, x)).Aggregate(this, AddReferenceByPath);
         }
 
-        /// <summary>
-        /// Adds a Reference to the <typeparamref name="T"/> <see cref="GetTypeAssembly{T}"/>
-        /// furnished <see cref="Assembly.Location"/>.
-        /// </summary>
-        /// <typeparam name="T"></typeparam>
-        /// <returns></returns>
+        /// <inheritdoc />
         public ResolveMetadataReferencesEventArgs AddReferenceToTypeAssembly<T>() => AddReferenceByPath(this, GetTypeAssembly<T>().Location);
 
-        /// <summary>
-        /// Adds a Reference to each of the <paramref name="paths"/>. Note, it is caller
-        /// responsibility in this instance to ensure that the Paths are correct.
-        /// </summary>
-        /// <param name="paths"></param>
-        /// <returns></returns>
+        /// <inheritdoc />
         public ResolveMetadataReferencesEventArgs AddReferencesByPaths(params string[] paths) => paths.Aggregate(this, AddReferenceByPath);
         // ReSharper restore UnusedMember.Global
     }
