e6f7f3eb217cde8b9867b8fc12edd393c07d8667
Michael W Powell <mwpowellhtx@gmail.com>
2019-06-13 16:44:19 -0400

    added keyword assets

---
+++ src/Code.Generation.Roslyn.Tests/Keywords.cs
@@ -0,0 +1,17 @@
+﻿namespace Code.Generation.Roslyn
+{
+    internal static class Keywords
+    {
+        // ReSharper disable InconsistentNaming
+        /// <summary>
+        /// &quot;ver&quot;
+        /// </summary>
+        public const string ver = nameof(ver);
+
+        /// <summary>
+        /// &quot;info&quot;
+        /// </summary>
+        public const string info = nameof(info);
+        // ReSharper restore InconsistentNaming
+    }
+}
