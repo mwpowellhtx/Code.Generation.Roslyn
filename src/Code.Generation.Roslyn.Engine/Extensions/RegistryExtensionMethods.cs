a722f30be954ea3735ed2c6964e0ff0e9e9ac883
Michael W Powell <mwpowellhtx@gmail.com>
2019-06-12 23:17:12 -0400

    added registry extension methods ensuring output directory exists

---
+++ src/Code.Generation.Roslyn.Engine/Extensions/RegistryExtensionMethods.cs
@@ -0,0 +1,18 @@
+﻿using System.IO;
+
+namespace Code.Generation.Roslyn
+{
+    internal static class RegistryExtensionMethods
+    {
+        public static TRegistry EnsureOutputDirectoryExists<TRegistry>(this TRegistry registry)
+            where TRegistry : IRegistrySet
+        {
+            if (!Directory.Exists(registry.OutputDirectory))
+            {
+                Directory.CreateDirectory(registry.OutputDirectory);
+            }
+
+            return registry;
+        }
+    }
+}
