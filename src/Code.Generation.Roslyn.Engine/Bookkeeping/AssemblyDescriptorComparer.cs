27c0e9b276224bcac5990e3e9a14ac454db4cae2
Michael W Powell <mwpowellhtx@gmail.com>
2019-06-18 20:11:14 -0400

    refactored descriptor code

---
+++ src/Code.Generation.Roslyn.Engine/Bookkeeping/AssemblyDescriptorComparer.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Code.Generation.Roslyn
+{
+    using static StringComparison;
+
+    public class AssemblyDescriptorComparer : IComparer<AssemblyDescriptor>
+    {
+        internal static AssemblyDescriptorComparer Comparer => new AssemblyDescriptorComparer();
+
+        /// <summary>
+        /// Private Constructor.
+        /// </summary>
+        private AssemblyDescriptorComparer() { }
+
+        /// <summary>
+        /// -1
+        /// </summary>
+        private const int LessThan = -1;
+
+        /// <summary>
+        /// 1
+        /// </summary>
+        private const int GreaterThan = 1;
+
+        public int Compare(AssemblyDescriptor x, AssemblyDescriptor y)
+            => x == null && y == null
+                ? LessThan
+                : x != null && y == null
+                    ? GreaterThan
+                    : x == null
+                        ? LessThan
+                        : string.Compare(x.AssemblyPath, y.AssemblyPath
+                            , InvariantCultureIgnoreCase);
+    }
+}
---
+++ src/Code.Generation.Roslyn.Engine/Bookkeeping/GeneratedSyntaxTreeDescriptorComparer.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Code.Generation.Roslyn
+{
+    using static StringComparison;
+
+    public class GeneratedSyntaxTreeDescriptorComparer : IComparer<GeneratedSyntaxTreeDescriptor>
+    {
+        /// <summary>
+        /// -1
+        /// </summary>
+        private const int LessThan = -1;
+
+        /// <summary>
+        /// 1
+        /// </summary>
+        private const int GreaterThan = 1;
+
+        /// <summary>
+        /// Internal Constructor.
+        /// </summary>
+        private GeneratedSyntaxTreeDescriptorComparer() { }
+
+        internal static GeneratedSyntaxTreeDescriptorComparer Comparer => new GeneratedSyntaxTreeDescriptorComparer();
+
+        public int Compare(GeneratedSyntaxTreeDescriptor x, GeneratedSyntaxTreeDescriptor y)
+            => x == null && y == null
+                ? LessThan
+                : x != null && y == null
+                    ? GreaterThan
+                    : x == null
+                        ? LessThan
+                        : string.Compare(x.SourceFilePath, y.SourceFilePath, InvariantCultureIgnoreCase);
+    }
+}
