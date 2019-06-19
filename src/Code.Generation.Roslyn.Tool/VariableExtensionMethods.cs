bb033714efdbfa8954be886fcb58b73eb9b94f84
Michael W Powell <mwpowellhtx@gmail.com>
2019-06-19 18:00:57 -0400

    added variable extension methods to the tooling

---
+++ src/Code.Generation.Roslyn.Tool/VariableExtensionMethods.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace Code.Generation.Roslyn
+{
+    using NConsole.Options;
+    using static String;
+
+    internal static class VariableExtensionMethods
+    {
+        /// <summary>
+        /// Returns whether <paramref name="variable"/> Has the <typeparamref name="T"/> Value.
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="variable"></param>
+        /// <param name="having"></param>
+        /// <returns></returns>
+        public static bool HasValue<T>(this Variable<T> variable, Func<T, bool> having) => having(variable);
+
+        private static bool IsNotNullOrEmpty(string s) => !IsNullOrEmpty(s);
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="variable"></param>
+        /// <returns></returns>
+        public static bool HasValue(this Variable<string> variable) => variable.HasValue(IsNotNullOrEmpty);
+    }
+}
