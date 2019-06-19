665b0732dbc30769516d245f67b4f62dc23667b6
Michael W Powell <mwpowellhtx@gmail.com>
2019-06-18 20:09:22 -0400

    added date time math

---
+++ src/Code.Generation.Roslyn.Engine/Extensions/DateTimeMath.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Code.Generation.Roslyn
+{
+    internal static class DateTimeMath
+    {
+        public static DateTime? Min(this DateTime? x, DateTime? y)
+            => x == null && y != null
+                ? y
+                : x != null && y == null
+                    ? x
+                    : x > y
+                        ? y
+                        : x;
+
+        public static DateTime? Max(this DateTime? x, DateTime? y) => Min(y, x);
+    }
+}
