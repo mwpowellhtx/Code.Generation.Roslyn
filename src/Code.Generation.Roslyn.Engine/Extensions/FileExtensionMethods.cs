9ef4f2807874fbd199ef176c95f351909144a501
Michael W Powell <mwpowellhtx@gmail.com>
2019-06-18 20:09:40 -0400

    added file extension methods

---
+++ src/Code.Generation.Roslyn.Engine/Extensions/FileExtensionMethods.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.IO;
+
+namespace Code.Generation.Roslyn
+{
+    internal static class FileExtensionMethods
+    {
+        public static DateTime? GetLastWriteTimeUtc(this string path)
+            => File.Exists(path) ? File.GetLastWriteTimeUtc(path) : (DateTime?) null;
+    }
+}
