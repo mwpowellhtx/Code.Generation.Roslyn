ccdd62202e39963bce1abbc499f5387388cc9165
Michael W Powell <mwpowellhtx@gmail.com>
2019-06-19 20:00:33 -0400

    refactoring shared bits approaching end to end integration testing

---
+++ src/tools/Shared/Baz.cs
@@ -0,0 +1,6 @@
+﻿namespace Foo
+{
+    public partial class Baz : Bar
+    {
+    }
+}
---
+++ src/tools/Shared/IBuz.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace Foo
+{
+    public partial interface IBuz : IDisposable
+    {
+    }
+}
