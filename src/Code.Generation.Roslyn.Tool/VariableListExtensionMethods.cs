5d75441bcbd333bb04d54e8759f6702011c9767d
Michael W Powell <mwpowellhtx@gmail.com>
2019-06-26 14:10:49 -0400

    refactored variable list sanitize operation

---
+++ src/Code.Generation.Roslyn.Tool/VariableListExtensionMethods.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Code.Generation.Roslyn
+{
+    using static String;
+
+    internal static class VariableListExtensionMethods
+    {
+        private static bool IsNotNullOrWhiteSpace(string s) => !IsNullOrWhiteSpace(s?.Trim());
+
+        public static IEnumerable<string> Sanitize(this IEnumerable<string> inputs) => inputs.Where(IsNotNullOrWhiteSpace).Select(x => x.Trim());
+    }
+}
