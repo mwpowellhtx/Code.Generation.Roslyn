83fbe7c6ef68249de029f0ea523efbebb8687dfb
Michael W Powell <mwpowellhtx@gmail.com>
2019-06-27 17:19:36 -0400

    updated test fixture base class

--- src/Code.Generation.Roslyn.Integration.Tests/ClassVerificationTestFixtureBase.cs
+++ src/Code.Generation.Roslyn.Integration.Tests/ClassVerificationTestFixtureBase.cs
@@ -6,7 +6,10 @@
         where T : TBase, new()
         where TBase : class
     {
-        protected T Instance => new T();
+        /// <summary>
+        /// Gets a New Instance of the <typeparamref name="T"/> Class.
+        /// </summary>
+        protected static T Instance => new T();
 
         protected ClassVerificationTestFixtureBase(ITestOutputHelper outputHelper)
             : base(outputHelper)
