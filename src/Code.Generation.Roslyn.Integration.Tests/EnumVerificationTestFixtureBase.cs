321c669b2b7a97453d710ca303bc39ab1ff0aac0
Michael W Powell <mwpowellhtx@gmail.com>
2019-06-30 12:26:01 -0400

    added enumeration based unit testing added fruit inspection test cases

---
+++ src/Code.Generation.Roslyn.Integration.Tests/EnumVerificationTestFixtureBase.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Foo
+{
+    using Xunit;
+    using Xunit.Abstractions;
+
+    /// <summary>
+    /// Performs some basic verification on <see cref="Enum"/> oriented types.
+    /// </summary>
+    /// <typeparam name="T"></typeparam>
+    public abstract class EnumVerificationTestFixtureBase<T> : TestFixtureBase
+        where T : struct
+    {
+        /// <summary>
+        /// The Enumerated <see cref="Type"/>.
+        /// </summary>
+        protected Type EnumType { get; } = typeof(T);
+
+        protected EnumVerificationTestFixtureBase(ITestOutputHelper outputHelper)
+            : base(outputHelper)
+        {
+        }
+
+        /// <summary>
+        /// Verifies that the Type is in fact set.
+        /// </summary>
+        [Fact]
+        public void Type_Is_Set() => EnumType.AssertNotNull();
+
+        /// <summary>
+        /// Asserts that the <see cref="Type.IsEnum"/>.
+        /// </summary>
+        [Fact]
+        public void Type_Is_Enum() => EnumType.AssertNotNull().AssertTrue(x => x.IsEnum);
+
+        /// <summary>
+        /// Gets the Values.
+        /// </summary>
+        protected virtual IEnumerable<T> Values
+        {
+            get
+            {
+                Type_Is_Enum();
+
+                foreach (T x in Enum.GetValues(EnumType))
+                {
+                    yield return x;
+                }
+            }
+        }
+    }
+}
---
+++ src/Code.Generation.Roslyn.Integration.Tests/FruitInspectionTests.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace Foo
+{
+    using Xunit;
+    using Xunit.Abstractions;
+    using static FruitKind;
+
+    /// <summary>
+    /// In keeping with the other Code Generation integration tests, first the code should
+    /// build. Next, we should be able to verify some aspects of the generated code itself
+    /// that are obvious and easy to identifier.
+    /// </summary>
+    public class FruitInspectionTests : EnumVerificationTestFixtureBase<FruitKind>
+    {
+        public FruitInspectionTests(ITestOutputHelper outputHelper)
+            : base(outputHelper)
+        {
+        }
+
+        /// <summary>
+        /// Gets the <see cref="EnumVerificationTestFixtureBase{T}.Values"/>
+        /// and verifies a couple of key aspects about that collection.
+        /// </summary>
+        protected override IEnumerable<FruitKind> Values => base.Values.AssertTrue(x => x.Count() == 6);
+
+        /// <summary>
+        /// Verifies that we have the Kind of Fruit we are expecting.
+        /// </summary>
+        /// <param name="actual"></param>
+        [Theory
+         , InlineData(Apple)
+         , InlineData(Orange)
+         , InlineData(Banana)
+         , InlineData(Lime)
+         , InlineData(Lemon)
+         , InlineData(Cherry)]
+        public void Verify_Kind(FruitKind actual) => Values.AssertContains(x => x == actual);
+    }
+}
