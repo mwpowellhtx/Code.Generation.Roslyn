44ae8a8dd5ba3e9dc31ce976faeb43a17bec1a88
Michael W Powell <mwpowellhtx@gmail.com>
2019-08-31 18:59:30 -0400

    added test fixture base class

---
+++ src/Code.Generation.Roslyn.Engine.Tests/TestFixtureBase.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections.Generic;
+
+namespace Code.Generation.Roslyn
+{
+    using Xunit.Abstractions;
+
+    public abstract class TestFixtureBase
+    {
+        protected ITestOutputHelper OutputHelper { get; }
+
+        protected TestFixtureBase(ITestOutputHelper outputHelper)
+        {
+            OutputHelper = outputHelper;
+        }
+
+        /// <summary>
+        /// Returns a fresh <see cref="IEnumerable{T}"/> corresponding to the
+        /// <paramref name="values"/>.
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="values"></param>
+        /// <returns></returns>
+        protected static IEnumerable<T> GetRange<T>(params T[] values)
+        {
+            // ReSharper disable once LoopCanBeConvertedToQuery
+            foreach (var x in values)
+            {
+                yield return x;
+            }
+        }
+    }
+}
