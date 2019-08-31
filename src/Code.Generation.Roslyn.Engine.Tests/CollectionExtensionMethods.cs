95d9765f8191097b815ef8d98d75f2fc19b250a6
Michael W Powell <mwpowellhtx@gmail.com>
2019-08-31 19:00:17 -0400

    added collection extension methods

---
+++ src/Code.Generation.Roslyn.Engine.Tests/CollectionExtensionMethods.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace Code.Generation.Roslyn
+{
+    internal static class CollectionExtensionMethods
+    {
+        public static IReadOnlyCollection<T> ToReadOnlyCollection<T>(this IEnumerable<T> values) => values.ToArray();
+    }
+}
