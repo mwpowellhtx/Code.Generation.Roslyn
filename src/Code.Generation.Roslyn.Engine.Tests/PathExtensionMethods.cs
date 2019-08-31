f82ddf88f877ca332a06c425378463159c44817a
Michael W Powell <mwpowellhtx@gmail.com>
2019-08-31 19:00:01 -0400

    added path extension methods

---
+++ src/Code.Generation.Roslyn.Engine.Tests/PathExtensionMethods.cs
@@ -0,0 +1,11 @@
+﻿using System.IO;
+using System.Linq;
+
+namespace Code.Generation.Roslyn
+{
+    internal static class PathExtensionMethods
+    {
+        public static string CombinePaths(this string basePath, params string[] otherPaths)
+            => Path.Combine(new[] {basePath}.Concat(otherPaths).ToArray());
+    }
+}
