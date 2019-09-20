a4da94b311562a0d549efb9cc8807d49dfa68330
Michael W Powell <mwpowellhtx@gmail.com>
2019-09-20 12:55:23 -0400

    reconsidered how better to handle the JSON serialization issue

--- src/Code.Generation.Roslyn.Engine.Services/Callbacks/ObjectToDataTransferObjectConversion.cs
+++
@@ -1,6 +0,0 @@
-﻿namespace Code.Generation.Roslyn
-{
-    public delegate TDataTransferObject ObjectToDataTransferObjectCallback<in T, out TDataTransferObject>(T obj);
-
-    public delegate T DataTransferObjectToObjectCallback<in TDataTransferObject, out T>(TDataTransferObject dataTransferObj);
-}
